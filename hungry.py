bfdebd09bd23d7f6e6d658eb49c4c60f3ed6a07a
thescorpionsting1011 <ashishyadav229@gmail.com>
2026-09-04 15:03:00 +0530

    water

--- hungry.py
+++ hungry.py
@@ -2,4 +2,7 @@ hungry=input("Are you hungry?")
 if hungry == "yes":
     print("eat samosa")
 else:
-    print("do your homework")
+    thirsty=input("Are you thirsty?")
+    if thirsty == "yes":
+        print("drink water")
+
